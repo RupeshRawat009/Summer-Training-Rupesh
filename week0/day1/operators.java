ff860f4d65ead4eb7dde16a37ff060931271586e
Rupesh Rawat <rupeshrawat133@gmail.com>
2024-07-11 14:07:45 +0530

    Second Commit

---
+++ week0/day1/operators.java
@@ -0,0 +1,10 @@
+package day1;
+public class operators {
+    
+    public static void main(String[] args) {
+       int a=69;
+       int b=75;
+       int sum=a+b;
+       System.out.println(sum);
+    }
+}
